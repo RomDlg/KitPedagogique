b7cad7eb4ea74d3028ae7077d92aec8ca6e0bf97
Doryan <doryanroumaneix123@gmail.com>
2021-10-08 15:37:13 +0200

    Ajout Commentaire

--- components/Query.js
+++ components/Query.js
@@ -5,6 +5,7 @@ import { ApolloClient, HttpLink, InMemoryCache, useQuery, gql} from '@apollo/cli
 import Seance from './Seance';
 
 
+// Permet d'appeler l'api du serveur (ApolloClient)
 const client = new ApolloClient({
     link: new HttpLink({
       uri: 'http://192.168.1.8:3301/api',
@@ -12,28 +13,9 @@ const client = new ApolloClient({
     cache: new InMemoryCache()
     });
   
- 
-function Donne(datas)
-{
-  Aretourner = "";
-   datas.getAllSeancesOrderByEtat.array.forEach(element => {
-     console.log("element : " + element);
-     Aretourner = "Seance";
-     Aretourner += element.id;
-     Aretourner += element.classeEcole;
-     Aretourner += element.date;
-     Aretourner += element.etat;
-     Aretourner ;
-   });
-   console.log(Aretourner);
-   return Aretourner;
-}
 
 
-export default function Query() {
-
-const [donne, SetData] = useState("");
-
+// Requete (GraphQl) permet d'avoir toutes les séances 
 const QuerySeance = gql`
 query{
   getAllSeancesOrderByEtat{
@@ -46,20 +28,45 @@ query{
   }
 }`;
 
+// Function qui appelle le useQuery 
+Donne = async () => 
+{
+  // Permet de call l'api avec la requête demandée 
+  const {loading, error, data} = await useQuery(QuerySeance ,{client: client});
+  console.log("Donne : " + data.getAllSeancesOrderByEtat);
+  // Permet d'avoir le JSON plus facilement
+  if(data != undefined)
+  {
 
-  const GetData = async () => {
-    const {loading, error, data} = await useQuery(QuerySeance ,{client: client});
-    console.log(data);
-    // const d = Donne(data.getAllSeancesOrderByEtat);
-    // console.log(d)
-    SetData(data.getAllSeancesOrderByEtat[0].lieu); 
+  const myObjStr = JSON.stringify(data);
+  console.log(myObjStr.getAllSeancesOrderByEtat);
+  if(data != undefined)
+  {
+    console.log(myObjStr.getAllSeancesOrderByEtat[0].id);
+  Seance(myObjStr.getAllSeancesOrderByEtat[0].id);
 }
 
-GetData();
+  }
+  //SetData(myObjStr);
+}
 
-  if(donne != null)
-  return(  
-      <Seance data={donne} />
-  )
 
+
+
+export default function Query() {
+
+
+const [donne, SetData] = useState("");
+  
+const d = Donne();
+console.log("Hello : " +  d.toString());
+
+
+console.log("Donne : " + donne);
+
+// Appelle Seance qui prend en parametre le résultat de data pour être traitée dans Seance
+return(
+  <>
+  </>
+)
 }
--- components/Seance.js
+++ components/Seance.js
@@ -1,23 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
-
+// Seance qui permet de gérer l'affichage de toute les séances 
 export default function Seance({data})
 {
-
+    console.log("Seance :" +  data + " Fin");
+    // Traitement des données
     const [idseance, SetSeanceId] = useState([]);
-    console.log("Seance :" +  data);
+   
     useEffect(() => {
-        SetSeanceId(data);
+        
     }, [data])
 
-
     return(
-        <SafeAreaView>
-            <Text style={{textAlign: 'center'}}>
-                {data}
-            </Text>
-        </SafeAreaView>
+        <ScrollView horizontal={true}>
+           
+        </ScrollView>
     )
 }
+
+// Le style de L'application
+const styles = StyleSheet.create({
+    Seance : {
+        height: 150,
+        width: 150,
+        borderColor: "#C8B0B0",
+        borderWidth: 2,
+        borderRadius: 20,
+        marginLeft: 20,
+        marginTop: 20,
+        display: 'flex',
+    }
+})
